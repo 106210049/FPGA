4e9c4faba1758250e6c01b41686cac24c6d140ee
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2023-07-11 23:27:22 +0700

    Update logic_gate.v

--- logic_gate.v
+++ logic_gate.v
@@ -1,5 +1,20 @@
+// NOT gate
 module logic_gate(in1,out);
 input in1;
 output out;
 assign out=~in1;
+endmodule
+
+// OR gate
+module logic_gate(in1,in2,out);
+input in1,in2;
+output out;
+assign out=in1|in2;
+endmodule
+
+// XOR gate
+module logic_gate(in1,in2,out);
+input in1,in2;
+output out;
+assign out=in1^in2;
 endmodule
